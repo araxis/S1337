5d3fe3fc37bf212c62b9638e9e65940a8c7f5cd8
meisam alifallahi <meisamalifallahi@hotmail.com>
2022-04-23 16:35:55 +0430

    fix arrange section

--- S1337.Test/UrlDownloaderTests.cs
+++ S1337.Test/UrlDownloaderTests.cs
@@ -21,10 +21,13 @@ public class UrlDownloaderTests
         var url = "http://1337.tech/images/a.jpg";
         var destination = @"D:\site";
         var onDiskPath = @"D:\site\images\a.jpg";
-        var fileSystem = new Mock<IFileSystem>();
-        fileSystem.Setup(f => f.File.Open(onDiskPath, FileMode.Create))
+        var mockFileSystem = new Mock<IFileSystem>();
+        mockFileSystem.Setup(f => f.File.Open(onDiskPath, FileMode.Create))
             .Returns(new MemoryStream())
             .Verifiable();
+
+        mockFileSystem.Setup(f => f.Path.GetDirectoryName(onDiskPath)).Returns(@"D:\site\images");
+        mockFileSystem.Setup(f => f.Directory.CreateDirectory(@"D:\site\images"));
         var handler = new Mock<HttpMessageHandler>();
         var client = handler.CreateClient();
         handler.SetupRequest(HttpMethod.Get, url).ReturnsResponse(url, "text/html");
@@ -32,14 +35,15 @@ public class UrlDownloaderTests
         filePathResolver.Setup(fp => fp.ResolvePath(It.IsAny<string>(), It.IsAny<string>(), It.IsAny<string>()))
             .Returns(onDiskPath);
 
-        var downloader = new UrlDownloader(client, filePathResolver.Object, fileSystem.Object);
+        var fileSystem=mockFileSystem.Object;
+        var downloader = new UrlDownloader(client, filePathResolver.Object, fileSystem);
         var resultStream = downloader.Download(url, destination, default);
         var reports = new List<DownloadState>();
         await foreach (var report in resultStream)
         {
             reports.Add(report);
         }
-        fileSystem.Verify(f => f.File.Open(onDiskPath,FileMode.Create), Times.Once);
+        mockFileSystem.Verify(f => f.File.Open(onDiskPath,FileMode.Create), Times.Once);
         reports.Should().NotBeEmpty();
     }
 }
