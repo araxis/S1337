ef73e0448afe97b5bf0292b4e5bc0230ed4d9488
meisam alifallahi <meisamalifallahi@hotmail.com>
2022-04-23 10:19:02 +0430

    Add project files.

---
+++ S1337/Program.cs
@@ -0,0 +1,26 @@
+﻿
+Console.WriteLine("Hello, World!");
+//My first impression is as follows 
+
+//var httpClient = new HttpClient();
+//var scanner = new Scanner(httpClient, new UrlFinder(), new RequestUrlbuilder());
+//var downloder = new ContentDownloader(new RequestUrlbuilder(), new FilePathResolver(), httpClient);
+//var links = new List<string>();
+//await foreach (var item in scanner.Scan("http://1337.tech"))
+//{
+//    links.Add(item);
+
+//}
+
+//foreach (var link in links)
+//{
+
+//    await foreach (var state in downloder.Download(link, @"D:\site2", default))
+//    {
+//        //update presentation here
+//    }
+//}
+
+//I'm going to use a library fore console ui design,maybe Terminal.Gui
+// and of course using dependency injection
+// and maybe add cli.
