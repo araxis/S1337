82f08eb26b47d5ef9d1eb54eadebc7fcbe4b38d2
meisam alifallahi <meisamalifallahi@hotmail.com>
2022-04-23 15:55:26 +0430

    built gui

--- S1337/Program.cs
+++ S1337/Program.cs
@@ -1,26 +1,82 @@
 ﻿
-Console.WriteLine("Hello, World!");
-//My first impression is as follows 
+using Microsoft.Extensions.DependencyInjection;
+using S1337;
+using S1337.Core;
+using Spectre.Console;
 
-//var httpClient = new HttpClient();
-//var scanner = new Scanner(httpClient, new UrlFinder(), new RequestUrlbuilder());
-//var downloder = new ContentDownloader(new RequestUrlbuilder(), new FilePathResolver(), httpClient);
-//var links = new List<string>();
-//await foreach (var item in scanner.Scan("http://1337.tech"))
-//{
-//    links.Add(item);
 
-//}
+var serviceProvider = new ServiceCollection().AddAppServices()
+                                             .BuildServiceProvider();
 
-//foreach (var link in links)
-//{
 
-//    await foreach (var state in downloder.Download(link, @"D:\site2", default))
-//    {
-//        //update presentation here
-//    }
-//}
 
-//I'm going to use a library fore console ui design,maybe Terminal.Gui
-// and of course using dependency injection
-// and maybe add cli.
+AnsiConsole.Write(new FigletText("1337.tech").Centered().Color(Color.Green));
+AnsiConsole.Write(new FigletText("Araxis").Centered().Color(Color.Red));
+AnsiConsole.MarkupLine("[bold yellow]please enter full[/] [red]url![/]");
+AnsiConsole.MarkupLine("if you are 1337, can leave it blank :)");
+var url = AnsiConsole.Ask<string>("site url[green][/] :", "http://1337.tech");
+if (string.IsNullOrWhiteSpace(url)) url = "http://1337.tech";
+while (!Uri.IsWellFormedUriString(url, UriKind.Absolute))
+{
+    url = AnsiConsole.Ask<string>("site [green]url[/] :");
+}
+
+
+
+
+var downloadLinks = new List<ScanResult>();
+var scanner = serviceProvider.Scanner();
+await AnsiConsole.Status().StartAsync("[yellow]Scanning[/]", async ctx =>
+{
+    await foreach (var item in scanner.Scan("http://1337.tech"))
+    {
+        downloadLinks.Add(item);
+        WriteScanResult(item);
+        ctx.Status($"[yellow] ({downloadLinks.Count}) Scanning[/]");
+    }
+    //ctx.Status($"[yellow] ({downloadLinks.Count}) Scanned[/]");
+});
+
+
+var download = AnsiConsole.Confirm("Download Site?");
+if (download)
+{
+    var path = AnsiConsole.Ask<string>("enter save folder [green]path[/].");
+    while (!path.IsDirectory())
+    {
+        path = AnsiConsole.Ask<string>("enter save folder [green]path[/].");
+    }
+
+    var downloader = serviceProvider.UrlDownloader();
+    await AnsiConsole.Progress().AutoClear(false)
+        .Columns(new ProgressColumn[]
+        {
+
+            new TaskDescriptionColumn(),    // Task description
+            new ProgressBarColumn(),        // Progress bar
+            new PercentageColumn(),         // Percentage
+            new SpinnerColumn(),            // Spinner
+        }).StartAsync(async ctx =>
+        {
+            var validLinks = downloadLinks.Where(c => c.State == ScanState.Success);
+
+            foreach (var link in validLinks)
+            {
+                var task = ctx.AddTask($"[green]{link.Url}[/]");
+                await foreach (var state in downloader.Download(link.Url, path, default))
+                {
+                    task.Increment(state.Percentage);
+                }
+            }
+
+        });
+}
+
+
+
+
+void WriteScanResult(ScanResult result)
+{
+    var color = result.State == ScanState.Success ? "green" : "red";
+    AnsiConsole.MarkupLine($"[{color}]*:[/] {result.Url}[{color}]...[/]");
+}
